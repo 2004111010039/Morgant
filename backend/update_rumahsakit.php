2ea5dc8fb16766d1d4ccc54b69bef3eb2274e6f6
Haiqal0 <85382998+Haiqal0@users.noreply.github.com>
2022-05-26 11:06:33 +0700

    Update update_rumahsakit.php

--- backend/update_rumahsakit.php
+++ backend/update_rumahsakit.php
@@ -6,8 +6,10 @@ $no_tlpn_rs = $_POST['no_tlpn_rs'];
 $alamat = $_POST['alamat'];
 $link_gmaps = $_POST['link_gmaps'];
 $jumlah_ambulan = $_POST['jumlah_ambulan'];
- 
-$sql = "UPDATE rumah_sakit set profil_rs='$profil_rs', no_tlpn_rs='$no_tlpn_rs', alamat='$alamat', link_gmaps='$link_gmaps', jumlah_ambulan='$jumlah_ambulan' where id='$id'";
+$lat = $_POST['latitude'];
+$long = $_POST['longtitude'];
+
+$sql = "UPDATE rumah_sakit set profil_rs='$profil_rs', no_tlpn_rs='$no_tlpn_rs', alamat='$alamat', link_gmaps='$link_gmaps', jumlah_ambulan='$jumlah_ambulan', lat = '$lat', lon='$long' where id='$id'";
  
 
 if(mysqli_query($koneksi, $sql)){
